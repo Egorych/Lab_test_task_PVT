bcb9bd53a09892d741f06e6e0f0b19669117fa1c
Egor Sankov <egor.v.sankov@gmail.com>
2018-03-22 10:07:25 +0300

    #1 Input of the number sequence-added sequence length check

--- LabTestTaskPVT/src/pvt/test/DataInput.java
+++ LabTestTaskPVT/src/pvt/test/DataInput.java
@@ -7,35 +7,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataInput {
-
 	public static ArrayList<Integer> sequenceReading() {
-
+		boolean flag = false;
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		List<Integer> sequenceOfNumbers;
+
 		try {
-			System.out.println("Enter the sequence of numbers via space:");
-			String strOfNumbers = reader.readLine();
-			List<Integer> sequenceOfNumbers = new ArrayList<Integer>();
-			if (strOfNumbers.length() != 0) {
-				for (String number : strOfNumbers.split(" ")) {
-					try {
-						sequenceOfNumbers.add(Integer.parseInt(number));
-					} catch (NumberFormatException e) {
-						System.out.println("You have to enter only integers!!");
+			do {
+				System.out.println("Enter the sequence of numbers via space:");
+				String strOfNumbers = reader.readLine();
+				String[] strOfNumbersSplit = strOfNumbers.split(" ");
+				sequenceOfNumbers = new ArrayList<Integer>();
+				if (strOfNumbers.length() != 0) {
+					if (strOfNumbersSplit.length < 51) {
+						for (String number : strOfNumbersSplit) {
+							try {
+								sequenceOfNumbers.add(Integer.parseInt(number));
+								flag = true;
+							} catch (NumberFormatException e) {
+								System.out.println("You have to enter only integers!!");
+								strOfNumbers = "";
+								flag = false;
+							}
+						}
+
+					} else {
+						System.out.println("The sequence can't include more than 50 numbers! Try again");
 						DataInput.sequenceReading();
 					}
+				} else {
+					System.out.println("You have to enter one number at least in sequence!!");
+					DataInput.sequenceReading();
 				}
-
-			} else {
-				System.out.println("You have to enter one number at least in sequence!!");
-				DataInput.sequenceReading();
-			}
+			} while (!flag);
 			return (ArrayList<Integer>) sequenceOfNumbers;
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
-
 	}
-
 }
