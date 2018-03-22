283fbdd2958b8d230f9b395514f1713b786a7efa
Egor Sankov <egor.v.sankov@gmail.com>
2018-03-22 10:27:39 +0300

    #2 Created Class DanceFloor

---
+++ LabTestTaskPVT/src/pvt/test/DanceFloor.java
@@ -0,0 +1,15 @@
+package pvt.test;
+
+public class DanceFloor {
+
+	private int sizeOfDanceFloor;
+
+	public int getSizeOfDanceFloor() {
+		return sizeOfDanceFloor;
+	}
+
+	public void setSizeOfDanceFloor(int sizeOfDanceFloor) {
+		this.sizeOfDanceFloor = sizeOfDanceFloor;
+	}
+
+}
